785d0f053b0ddcad908b13a42d1d29632a89698a
maxus2205 <maxus.work@ukr.net>
2017-09-28 22:50:17 +0300

    copy prog to projectsite

--- projectsite/script.js
+++ projectsite/script.js
@@ -1,108 +1,114 @@
-var myTable = document.getElementById("myTable");
+var tableBlock = document.querySelector('.table-block');
+var table = document.querySelector('.table');
 
-var leftTab = document.getElementById("left-func");
-var rightTab = document.getElementById("right-func");
-var topTab = document.getElementById("top-func");
-var botTab = document.getElementById("bot-func");
 
-var addCol = document.getElementById("addCol");
-var addRow = document.getElementById("addRow");
+var delColBut = document.querySelector('.button_del-col');
+var delRowBut = document.querySelector('.button_del-row');
+var addColBut = document.querySelector('.button_add-col');
+var addRowBut = document.querySelector('.button_add-row');
 
-var r;
-var c;
+var myRow;
+var myColumn;
 
-addCol.onclick = addColFunc;
+addColBut.addEventListener("click", addColFunc);
+addRowBut.addEventListener("click", addRowFunc);
+delColBut.addEventListener("click", delColFunc);
+delRowBut.addEventListener("click", delRowFunc);
 
-function addColFunc() {
-  for (var i = 0; i < myTable.rows.length; i++) {
-    myTable.rows[i].insertCell(-1);
-  }
-  topTab.rows[0].insertCell(-1);
-  botTab.rows[0].insertCell(-1);
+function clearTable() {
+    var allCells = document.querySelectorAll("td");
+    for (i = 0; i < allCells.length; i++) {
+        allCells[i].innerHTML = "";
+    }
 }
 
-addRow.onclick = addRowFunc;
+//clearTable(); //clear table if its no empty  (optional)
+
+//initialization buttons
+table.addEventListener("mouseover", showBut);
+tableBlock.addEventListener("mouseleave", delButtonsHide);
+
+function showBut(event) {
+    if (!(event.target instanceof HTMLTableCellElement)) {
+        return;
+    }
+    var target = event.target;
+    //target.style.background = 'pink';
+    with(event.target || event.srcElement) {
+        myRow = parentNode.rowIndex;
+        myColumn = cellIndex;
+        // target.innerHTML = "row: " + myRow + ", cell: " + myColumn;
+    }
+
+    setDelButPosition();
+
+    delColBut.style.transition = "300ms";
+    delRowBut.style.transition = "300ms";
+
+    if (table.rows.length > 1) {
+        delRowBut.style.display = "block"
+    } else delButtonsHide;
+    if (table.rows[0].cells.length > 1) {
+        delColBut.style.display = "block"
+    } else delButtonsHide;
+
+    //Changing the transition duration:
+    table.addEventListener("mouseleave", transitionInTabOff);
+}
+
+function delButtonsHide() {
+    delColBut.style.display = "none";
+    delRowBut.style.display = "none";
+}
+
+// Setting up position of delete buttons
+function setDelButPosition() {
+    delColBut.style.left = 3 + 51 * myColumn + "px";
+    delRowBut.style.top = 3 + 51 * myRow + "px";
+}
+
+function transitionInTabOff() {
+    delColBut.style.transition = "";
+    delRowBut.style.transition = "";
+}
+
+//Optional block to hide the row and column number (if show functions are on):
+/*table.onmouseout = function(event) {
+    if (!(event.target instanceof HTMLTableCellElement)) {
+        return;
+    }
+    var target = event.target;
+    target.style.background = '';
+    target.innerHTML = "";
+}*/
+
+
+// Button functions:
+function addColFunc() {
+    var row = document.createElement("tr");
+    table.appendChild(row);
+    for (var i = 0; i < table.rows[0].cells.length; i++) {
+        row.insertCell(i);
+    }
+}
 
 function addRowFunc() {
-  var row = document.createElement("tr");
-  myTable.appendChild(row);
-  for (var i = 0; i < myTable.rows[0].cells.length; i++) {
-    row.insertCell(i);
-  }
-  leftTab.insertRow(-1).insertCell(0);
-  rightTab.insertRow(-1).insertCell(0);
+    for (var i = 0; i < table.rows.length; i++) {
+        table.rows[i].insertCell(-1);
+    }
 }
 
 function delColFunc() {
-  console.log("c    myColumn is: " + c);
-  console.log("c    myRow is: " + r);
-
-  for (var i = 0; i < myTable.rows.length; i++) {
-    myTable.rows[i].deleteCell(c);
-  }
-  topTab.rows[0].deleteCell(-1);
-  botTab.rows[0].deleteCell(-1);
-
-  hideRemoveButtons();
+    // console.log("Row:" + myRow + ";Col:" + myColumn);
+    for (var i = 0; i < table.rows.length; i++) {
+        table.rows[i].deleteCell(myColumn);
+    }
+    delButtonsHide();
 }
 
 function delRowFunc() {
-  console.log("r    myRow is: " + r);
-  console.log("c    myColumn is: " + c);
-
-  myTable.deleteRow(r);
-  leftTab.deleteRow(-1);
-  rightTab.deleteRow(-1);
-
-  hideRemoveButtons();
-}
-
-function hideRemoveButtons() {
-  var firstRowInTopTable = topTab.rows[0];
-  for (var i = 0; i < firstRowInTopTable.cells.length; i++) {
-    remoteAttributesForCell(firstRowInTopTable.cells[i]);
-  }
-  for (i = 0; i < leftTab.rows.length; i++) {
-    remoteAttributesForCell(leftTab.rows[i].cells[0]);
-  }
-}
-
-function remoteAttributesForCell(targetCell) {
-  targetCell.innerHTML = "";
-  targetCell.removeAttribute("class");
-  targetCell.removeAttribute("id");
-  targetCell.removeAttribute("onclick");
-}
-
-bigTable.onmouseleave = hideRemoveButtons;
-myTable.onmouseover = showBut;
-
-function showBut(event) {
-  if (!(event.target instanceof HTMLTableCellElement)) {
-    return;
-  }
-  with(event.target || event.srcElement) {
-    var myRow = parentNode.rowIndex;
-    var myColumn = cellIndex;
-  }
-
-  r = myRow;
-  c = myColumn;
-
-  hideRemoveButtons();
-
-  if (myTable.rows[0].cells.length > 1) {
-    setOnClickActionForCell(topTab.rows[0].cells[myColumn], "del", "delCol", delColFunc);
-  }
-  /*else alert("Can't do this, you have only one column!");*/
-  if (myTable.rows.length > 1) {
-    setOnClickActionForCell(leftTab.rows[myRow].cells[0], "del", "delRow", delRowFunc);
-  }
-  /*else alert("Can't do this, you have only one row!");*/
-}
-
-function setOnClickActionForCell(targetCell, targetClassName, targetId, targetFunc) {
-  targetCell.className = targetClassName;
-  targetCell.id = targetId;
-  targetCell.onclick = targetFunc;
+    // console.log("r:" + myRow);
+    // console.log("before" + table.rows.length);
+    table.deleteRow(myRow);
+    delButtonsHide();
 }
